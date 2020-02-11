cba391308a1cfca5b57278be065563ce36cfa72b
mclemen <michael.clemen@gmail.com>
2020-02-11 16:38:21 +0800

    edit pagination code

--- mongodb_wp.php
+++ mongodb_wp.php
@@ -31,59 +31,74 @@ defined( 'ABSPATH' ) or die('Nothing to see here!');
 
 
 function display_db() {
-	$manager = new MongoDB\Driver\Manager("mongodb://host:27017");
+	$manager = new MongoDB\Driver\Manager("mongodb://server_ip:27017");
 	
 	$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
 	$perPage = isset($_GET['per-page']) && $_GET['per-page'] <= 50 ? (int)$_GET['per-page'] : 319;
 
 	$start = ($page > 1) ? ($page * $perPage) - $perPage : 0;
 	$options = [
-	    "limit" => $perPage,
-	    "skip" => $start
+		"limit" => $perPage,
+		"skip" => $start
 	];
 
-	$collection = 'dbname.collection_name';
+	$firstPage = 1;
+	$nextPage = $page + 1;
+	$previousPage = $page - 1;
+
+	$collection = 'dbname.collections';
 	$query = new MongoDB\Driver\Query([], $options);
 	$cursor = $manager->executeQuery( $collection, $query);
 
-	$stats = new MongoDB\Driver\Command(["count" => "you_collection_here"]);
-    $res = $manager->executeCommand("dbname", $stats);
-    $total = ($res->toArray()[0]->n);
-    $pages = $total / $perPage;
+	$stats = new MongoDB\Driver\Command(["count" => "collections"]);
+	$res = $manager->executeCommand("dbname", $stats);
+	$total = ($res->toArray()[0]->n);
+	$pages = $total / $perPage;
 
 	?>
 	<table class="table table-bordered">
-  		<tbody><?php
-			foreach ($cursor as $row) {
-				echo "<tr>";
-				if( !empty( $row->league ) ) {
-					echo "<td style='width:65%;'><strong>League: <i>".implode($row->league)."</i></strong></td>";
-				}
-				echo "</tr>";
-				echo "<tr>";
-				if( !empty( $row->team ) ) {
-					echo "<tr><td style='width:50%;'>Teams: ".implode(", ", $row->team)."</td>";
-				}
-				if( !empty( $row->totalscore ) ) {
-					echo "<td>Total Score: ".implode($row->totalscore)."</td></tr>";
-				}
-				echo "</tr>";
+		<tbody><?php
+		foreach ($cursor as $row) {
+			echo "<tr>";
+			if( !empty( $row->league ) ) {
+				echo "<td style='width:65%;'><strong>League: <i>".implode($row->league)."</i></strong></td>";
 			}
+			echo "</tr>";
+			echo "<tr>";
+			if( !empty( $row->team ) ) {
+				echo "<tr><td style='width:50%;'>Teams: ".implode(", ", $row->team)."</td>";
+			}
+			if( !empty( $row->totalscore ) ) {
+				echo "<td>Total Score: ".implode($row->totalscore)."</td></tr>";
+			}
+			echo "</tr>";
+		}
 		?>
-		</tbody>
-	</table>
-	<nav aria-label="Page navigation example">
-  		<ul class="pagination">
-		<?php for($x = 1; $x <= $pages; $x++): ?>
-			<li class="page-item"><a class="page-link" href="?page=<?php echo $x;?>" class="page-item"><?php echo $x?></a></li>
-		<?php endfor; ?>	
-		</ul>
-	</nav>
+	</tbody>
+</table>
+<nav aria-label="Page navigation example">
+			<ul class="pagination">
+				<?php if ($page > 1): ?>
+				<li class="prev page-item"><a class="page-link" href="?page=<?php echo $page-1 ?>">Prev</a></li>
+				<?php endif; ?>
 
-	<?php
+				<?php if ($page-2 > 0): ?><li class="page-item"><a class="page-link" href="?page=<?php echo $page-2 ?>"><?php echo $page-2 ?></a></li><?php endif; ?>
+				<?php if ($page-1 > 0): ?><li class="page-item"><a class="page-link" href="?page=<?php echo $page-1 ?>"><?php echo $page-1 ?></a></li><?php endif; ?>
+
+				<li class="currentpage page-item active"><a class="page-link" href="?page=<?php echo $page ?>"><?php echo $page ?></a></li>
+
+				<?php if ($page+1 < $pages +1): ?><li class="page-item"><a  class="page-link" href="?page=<?php echo $page+1 ?>"><?php echo $page+1 ?></a></li><?php endif; ?>
+	<?php if ($page+2 < $pages +1): ?><li class="page-item"><a  class="page-link" href="?page=<?php echo $page+2 ?>"><?php echo $page+2 ?></a></li><?php endif; ?>
+
+				<?php if ($page < $pages): ?>
+				<li class="next page-item"><a class="page-link" href="?page=<?php echo $page+1 ?>">Next</a></li>
+				<?php endif; ?>
+
+				
+			</ul>
+		</nav>
+<?php
 
 }
 
-add_shortcode('mongodb_wp', 'display_db');
-
-
+add_shortcode('srdb', 'display_db');
